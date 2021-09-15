764b656ad06bc3b36ca28b315c0ca85ac6aa5fe0
skj092 <sonujha092@gmail.com>
2021-09-15 17:01:05 +0530

    'updated'

--- app.py
+++ app.py
@@ -7,10 +7,19 @@ import re
 import numpy as np
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
+# 
+# from keras.models import load_model
+
+# import tensorflow as tf
+
 import tensorflow as tf
+import numpy as np
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
@@ -26,7 +35,7 @@ MODEL_PATH = 'model.h5'
 
 # Load your trained model
 
-model = load_model(MODEL_PATH)   
+model = models.load_model(MODEL_PATH)   
 model.make_predict_function()     
 print('Model loaded. Check http://127.0.0.1:5000/')
 
@@ -39,7 +48,7 @@ def model_predict(img_path):
     # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x, mode='caffe')
-    model = load_model('model.h5')
+    model = models.load_model('model.h5')
     preds = model.predict(x)
     score = tf.nn.softmax(preds[0])
     return np.argmax(score)
@@ -56,13 +65,11 @@ def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-
         # Make prediction
         preds = model_predict(file_path)
         return preds
